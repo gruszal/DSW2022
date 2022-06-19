9c2964b8b7126740fb4df8c670f76748301ab5b2
alekgruszczynski <gruszal@gmail.com>
2022-06-19 13:48:25 +0200

    zajęcia 02

---
+++ zajęcia02/05/dict1b.py
@@ -0,0 +1,10 @@
+osoba = {}
+osoba['imie'] = 'Guido'
+osoba['nazwisko'] = 'van Rossum'
+osoba['rok_urodzenia'] = 1906
+
+print(osoba)
+
+print(osoba['imie'])
+print(osoba['nazwisko'])
+print(osoba['rok_urodzenia'])
---
+++ zajęcia02/05/dict4a.py
@@ -0,0 +1,7 @@
+osoba = {'imie': 'Guido',
+         'nazwisko': 'van Rossum',
+         'rok_urodzenia': 1906}
+
+zawod = osoba['zawod']  # to nie zadziała
+
+print(zawod)
