4c8a538240476f1dfa4b7fd5749ff3692d9c36b8
alekgruszczynski <gruszal@gmail.com>
2022-06-19 13:44:17 +0200

    zajęcia 02

---
+++ zajęcia02/04/zadanie/rozw.py
@@ -0,0 +1,11 @@
+dane = input('Wpisz dowolną ilość wyrazów oddzielonych przecinkiem: ')
+
+lista = dane.split(',')
+
+print(lista)
+
+for wyraz in lista:
+    wyraz = wyraz.strip()
+
+    if wyraz:
+        print(f'Wyraz "{wyraz}" ma długość {len(wyraz)}')
