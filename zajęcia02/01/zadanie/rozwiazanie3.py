71f9fb415d95890be5860db24b9193dd0f741ec7
alekgruszczynski <gruszal@gmail.com>
2022-06-19 10:08:37 +0200

    zajęcia 01

--- zajęcia02/01/zadanie/rozwiazanie3.py
+++ zajęcia02/01/zadanie/rozwiazanie3.py
@@ -2,3 +2,5 @@ wartosci = [65, 35, 29, 60, 54, 9, 43, 83, 85, 68]
 
 suma = sum(wartosci)
 print(suma)
+
+# listę funkcji wbudowanych można znaleźć tutaj: https://docs.python.org/3/library/functions.html
