82a0c5ce19090314cc10b42ed5e470e713feb6a9
alekgruszczynski <gruszal@gmail.com>
2022-10-15 11:06:12 +0200

    zajęcia 03

---
+++ zajecia03/03/01_wyjatek1a.py
@@ -0,0 +1,9 @@
+print("Początek programu")
+
+try:
+    wynik_dzielenia_przez_zero = 13 / 0  # to jest linia, która wywołuje wyjątek
+    print("Ta linijka się nie wykona")
+except ZeroDivisionError:
+    print("Ktoś próbował dzielić przez zero, więc zamiast tego wypisuję ten komunikat")
+
+print('Program działa dalej')
