f1beca5d6007bf8c9b0e00499f458da1393a84a9
alekgruszczynski <gruszal@gmail.com>
2022-10-16 10:08:22 +0200

    zajęcia 04

---
+++ zajecia04/01/serializacja_csv1.py
@@ -0,0 +1,15 @@
+# https://docs.python.org/3/library/csv.html
+import csv
+from sklep import warzywa_i_owoce
+
+print(warzywa_i_owoce)
+
+plik = open('warzywa_i_owoce.csv', 'w')
+klucze = warzywa_i_owoce[0].keys()  # wyciągamy wszystkie klucze z pierwszego słownika na liście
+
+print(klucze)
+
+writer = csv.DictWriter(plik, fieldnames=klucze)
+
+writer.writeheader()
+writer.writerows(warzywa_i_owoce)
---
+++ zajecia04/01/serializacja_csv3.py
@@ -0,0 +1,14 @@
+# https://docs.python.org/3/library/csv.html
+import csv
+
+plik = open('slodycze.csv', 'r')
+
+reader = csv.DictReader(plik)  # czytnik przygotowany specjalnie dla list słowników z nagłówkiem
+
+slodycze = list(reader)
+
+print(slodycze)
+
+nazwa_pierwszego_art = slodycze[1]['nazwa']
+
+print(nazwa_pierwszego_art)
