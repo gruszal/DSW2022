0280c75c59bcb585f340a5d56a0a5315ab5a5505
alekgruszczynski <gruszal@gmail.com>
2022-10-29 11:12:02 +0200

    zajęcia 05

--- zajecia05/02/zapytania1a.py
+++ zajecia05/02/zapytania1a.py
@@ -3,6 +3,9 @@
 from urllib.request import urlopen
 import json
 
+import ssl  # https://clay-atlas.com/us/blog/2021/09/26/python-en-urllib-error-ssl-certificate/
+ssl._create_default_https_context = ssl._create_unverified_context
+
 with urlopen('http://yesno.wtf/api') as response:
     odpowiedz_json = response.read()
     print(odpowiedz_json)
