05c1f84749017e938e9705c98bf9ba83183bba90
alekgruszczynski <gruszal@gmail.com>
2022-10-29 12:52:59 +0200

    zajęcia 05

---
+++ zajecia05/03/tuple03.py
@@ -0,0 +1,7 @@
+liczba1, liczba2 = 'jeden', 'dwa'  # możemy ustawiać wartości dwóch zmiennych w jednej linii
+
+print(f'liczba1={liczba1}, liczba2={liczba2}')
+
+liczba1, liczba2 = liczba2, liczba1  # możemy zamienić zmienne wartościami
+
+print(f'liczba1={liczba1}, liczba2={liczba2}')
